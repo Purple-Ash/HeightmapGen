a5903cff4522425e596e550e10e8c3307172ec25
magistermaks <magistermaks@darktree.net>
2026-09-16 22:10:54 +0200

    Move from SDL to Winx+OpenGL (using glad)
    lmao

--- HeightmapGenerator/view/view.cpp
+++ HeightmapGenerator/view/view.cpp
@@ -1,8 +1,10 @@
 #include <vector>
 
-#include "SDL.h"
-
+#include <cstdint>
 #include "voronoi.hpp"
+
+#include <winx.h>
+#include <glad/glad.h>
 
 static float voronoi_sampler(int x, int y) {
 	VoronoiNoise noise;
@@ -28,21 +30,38 @@ struct Sampler {
 };
 
 
-void write_image_data(void* data, int w, int h, int pitch, Sampler::Function sampler) {
+void write_image_data(void* data, int w, int h, Sampler::Function sampler) {
 	uint8_t* pixels = static_cast<uint8_t*>(data);
 
 	for (int y = 0; y < h; y ++) {
 		for (int x = 0; x < w; x ++) {
-			uint8_t* pixel = pixels + x * 4;
 			const uint8_t normalized = static_cast<uint8_t>(255 * sampler(x, y));
 
-			pixel[0] = 255; // A
-			pixel[1] = normalized; // R
-			pixel[2] = normalized; // G
-			pixel[3] = normalized; // B
-		}
+			pixels[0] = normalized;
+			pixels[1] = normalized;
+			pixels[2] = normalized;
 
-		pixels += pitch;
+			pixels += 3;
+		}
+	}
+}
+
+static bool should_run = true;
+static int index = 0;
+
+static std::vector<Sampler> samplers = {
+	{voronoi_sampler, "Voronoi"},
+	{bitwise_sampler, "Bitwise"},
+	{white_sampler, "White"}
+};
+
+void window_close_handler() {
+	should_run = false;
+}
+
+void window_keyboard_handler(int state, int keycode) {
+	if (keycode == WXK_ENTER && state == WINX_RELEASED) {
+		index = (index + 1) % samplers.size();
 	}
 }
 
@@ -50,60 +69,44 @@ int main(int argc, char *argv[]) {
 	const int w = 800;
 	const int h = 800;
 
-	SDL_Window *window;
-	SDL_Renderer *renderer;
-	SDL_Event event;
+	void* pixels = malloc(3 * w * h);
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
-		return 3;
+	winxOpen(w, h, "Noise Viewer");
+	winxSetCloseEventHandle(window_close_handler);
+	winxSetKeyboardEventHandle(window_keyboard_handler);
+
+	gladLoadGL();
+
+	int current = -1;
+
+	GLuint texture = 0;
+	glCreateTextures(GL_TEXTURE_2D, 1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	GLuint read_fb;
+	glGenFramebuffers(1, &read_fb);
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, read_fb);
+	glFramebufferTexture2D(GL_READ_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);
+
+	while (should_run) {
+		winxPollEvents();
+
+		if (current != index) {
+			current = index;
+
+			write_image_data(pixels, w, h, samplers[index].function);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB,  GL_UNSIGNED_BYTE, pixels);
+
+			winxSetTitle(samplers[index].name);
+		}
+
+		glBlitFramebuffer(0, 0, w, h, 0, 0, w, h, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+		winxSwapBuffers();
 	}
 
-	if (SDL_CreateWindowAndRenderer(w, h, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
-		return 3;
-	}
-
-	SDL_Texture* buffer = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_BGRA8888, SDL_TEXTUREACCESS_STREAMING, w, h);
-
-	int index = 1;
-
-	std::vector<Sampler> samplers = {
-		{voronoi_sampler, "Voronoi"},
-		{bitwise_sampler, "Bitwise"},
-		{white_sampler, "White"}
-	};
-
-	while (true) {
-
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
-				return 0;
-			}
-
-			if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_TAB && event.key.repeat == 0) {
-				index = (index + 1) % samplers.size();
-
-				void* pixels;
-				int pitch;
-				SDL_LockTexture(buffer, nullptr, &pixels, &pitch);
-				write_image_data(pixels, w, h, pitch, samplers[index].function);
-				SDL_UnlockTexture(buffer);
-
-				SDL_SetWindowTitle(window, samplers[index].name);
-			}
-		};
-
-		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
-		SDL_RenderClear(renderer);
-		SDL_RenderCopy(renderer, buffer, nullptr, nullptr);
-
-		SDL_RenderPresent(renderer);
-	}
-
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
+	winxClose();
 	return 0;
 }
